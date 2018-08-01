1ac1bdd50d8c4e903d283a204b96f5a1d4d70a4d
nguyenthihailoan <loannth@zergitas.com>
2018-08-01 09:52:51 +0700

    creatbase_initapi youtube

---
+++ app/src/main/java/com/youtu/sleep/youtubebackground/widget/textviews/TextViewCustom.java
@@ -0,0 +1,33 @@
+package com.youtu.sleep.youtubebackground.widget.textviews;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.youtu.sleep.youtubebackground.utils.Contants;
+
+/**
+ * Created by DaiPhongPC on 7/31/2018.
+ */
+
+public class TextViewCustom extends TextView {
+    public TextViewCustom(Context context) {
+        super(context);
+    }
+
+    public TextViewCustom(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextViewCustom(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        applyCustomFont(context);
+    }
+
+    public void applyCustomFont(Context context) {
+        Typeface typeface = FontCache.getTypeface(context, Contants.NAME_FONT);
+        setTypeface(typeface);
+    }
+}
